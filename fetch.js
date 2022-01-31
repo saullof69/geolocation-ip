f6b044792b4f4f68115836e4effa106461dc5d3b
Saaulo7x <saaullorodrigues@gmail.com>
2022-01-31 14:17:30 -0300

    new update: sytle

--- fetch.js
+++ fetch.js
@@ -1,4 +1,4 @@
-const url = 'https://api.freegeoip.app/json/?apikey=cde289e0-8148-11ec-ae4d-9757438ff4e4';
+const url = 'https://api.freegeoip.app/json/?apikey=1951d6a0-82b4-11ec-ae7e-b9692bd78110';
 
 let user = {}
 
@@ -7,11 +7,11 @@ fetch(url)
         return response.json();
     })
     .then(result => {
-        user = result;
         document.getElementById('ip').textContent = result.ip;
         document.getElementById('city').textContent = result.region_code;
         document.getElementById('states').textContent = result.zip_code;
         document.getElementById('provider').textContent = result.time_zone;
+        document.getElementById('others').textContent = result.country_code + '  ' + result.latitude + '  ' + result.longitude + '  ' + result.metro_code;
     });
 //
 
